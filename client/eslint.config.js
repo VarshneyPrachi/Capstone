fb0732eaf38b5a8232764ac51c126c8c580c0f10
Prachi Varshney <142157900+VarshneyPrachi@users.noreply.github.com>
2026-08-24 21:55:25 +0530

    Update ESLint configuration with global ignores
    Add global ignores and extend ESLint configurations for JavaScript and React files.

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -3,6 +3,14 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
 
 
     ],
